b3b2bee74cd9c883760c539b30e97bf241523525
VITALIY1986 <staslego1986@gmail.com>
2024-09-23 18:45:09 +0300

    sdfhsdsdh

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -6,38 +6,62 @@ import TeethIcon from "@/assets/icons/Teeth.webp";
 import ReactYoutube from "@/components/react-youtube/ReactYoutube";
 
 interface AboutPageProps {
-	videoId: string;
-	youtubedescription: string;
+  videoId: string;
+  youtubedescription: string;
 }
 
 const AboutPage: FC<AboutPageProps> = ({ videoId,youtubedescription }) => {
-	return (
-		<div id="about" className={scss.about__page}>
-			<div className={scss.block__1}>
-				<div className="container">
-					<div className={scss.about__us}>
-						<Image
-							priority={true}
-							quality={1}
-							loading="eager"
-							className={scss.balance__for__mobile}
-							src={TeethIcon}
-							alt="Cabinet ORL"
-						/>
-						<h1 className={scss.title}>
-							<FormattedMessage id="page.about.title" />
-						</h1>
-						<div className={scss.video__text}>
-							<ReactYoutube videoId={videoId} className={scss.youtube} />
-							<p className={scss.text}>
-								{youtubedescription}
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <>
+      <div id="about" className={scss.about__page}>
+        <div className={scss.block__1}>
+          <div className="container">
+            <div className={scss.about__us}>
+              {/* Главное изображение */}
+              <Image
+                priority={true}
+                quality={1}
+                loading="eager"
+                className={scss.balance__for__mobile}
+                src={TeethIcon}
+                alt={"Cabinet ORL"}
+              />
+              {/* Иконки */}
+              <div className={scss.icon__first}>
+                <Image
+                  priority={true}
+                  quality={25}
+                  loading="eager"
+                  src={TeethIcon}
+                  alt={"Cabinet ORL Icon 1"}
+                />
+              </div>
+              <div className={scss.icon__second}>
+                <Image
+                  priority={true}
+                  quality={25}
+                  loading="eager"
+                  src={TeethIcon}
+                  alt={"Cabinet ORL Icon 2"}
+                />
+              </div>
+              {/* Заголовок */}
+              <h1 className={scss.title}>
+                <FormattedMessage id="page.about.title" />
+              </h1>
+              {/* Видео и текст */}
+              <div className={scss.video__text}>
+                <ReactYoutube videoId={videoId} className={scss.youtube} />
+                <p className={scss.text}>
+                 {youtubedescription}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutPage;
