cc834d8699c30c015a017fc86d57341a93a385c2
VITALIY1986 <staslego1986@gmail.com>
2024-09-23 18:15:17 +0300

    shhshd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,69 @@
 import React, { FC } from "react";
+import dynamic from 'next/dynamic';
 import Layout from "@/components/layout/Layout";
-import HomePage from "@/components/pages/HomePage";
-import AboutPage from "@/components/pages/AboutPage";
-import Service from "@/components/pages/Service";
-import ContactPage from "@/components/pages/ContactPage";
 import { getAllPosts } from "../../lib/api";
 import { GetStaticProps } from 'next';
 
-interface Post {
-	videoId: string;
-	slug: string;
-	title: string;
-	youtubedescription: string;
+// Динамическая загрузка компонентов
+const HomePage = dynamic(() => import('@/components/pages/HomePage'));
+const AboutPage = dynamic(() => import('@/components/pages/AboutPage'));
+const Service = dynamic(() => import('@/components/pages/Service'), { ssr: false }); // Отключаем SSR для этого компонента
+const ContactPage = dynamic(() => import('@/components/pages/ContactPage'));
 
-	coverImage: {
-		url: string; // Ensure this is an object with a url field
-	};
-	tags: { [key: string]: string }; // Update tags field as an object
+interface Post {
+  videoId: string;
+  slug: string;
+  title: string;
+  youtubedescription: string;
+  coverImage: {
+    url: string;
+  };
+  tags: { [key: string]: string };
 }
 
 interface Props {
-	posts: Post[];
+  posts: Post[];
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-	const posts = await getAllPosts(true);
-	
-	return {
-		props: {
-			posts,
-		},
-	};
+  const posts = await getAllPosts(true);
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
-// Function to filter posts by tag
+// Функция для фильтрации постов по тегу
 const filterPostsByTag = (posts: Post[], tagName: string): Post[] => {
-	return posts.filter((post) => {
-		const tagsArray = Object.values(post.tags || {}); // Convert object to array
-		return tagsArray.some(tag => tag === tagName); // Check for tag presence
-	});
+  return posts.filter((post) => {
+    const tagsArray = Object.values(post.tags || {});
+    return tagsArray.some(tag => tag === tagName);
+  });
 };
 
 const Index: FC<Props> = ({ posts }) => {
-	const heroPost = posts[0];
+  const heroPost = posts[0];
 
-	// Example of filtering posts by the "homepage" tag
-	const filteredPosts = filterPostsByTag(posts, "homepage");
+  // Пример фильтрации постов по тегу "homepage"
+  const filteredPosts = filterPostsByTag(posts, "homepage");
 
-	return (
-		<Layout>
-			{filteredPosts.length > 0 ? (
-				filteredPosts.map((post: Post) => (
-					<React.Fragment key={post.slug}>
-						<HomePage url={post.coverImage.url} />
-						<Service />
-						<AboutPage videoId={post.videoId} youtubedescription={post.youtubedescription}/>
-						<ContactPage />
-					</React.Fragment>
-				))
-			) : (
-				<p>No posts found with the specified tag.</p>
-			)}
-		</Layout>
-	);
+  return (
+    <Layout>
+      {filteredPosts.length > 0 ? (
+        filteredPosts.map((post: Post) => (
+          <React.Fragment key={post.slug}>
+            <HomePage url={post.coverImage.url} />
+            <Service />
+            <AboutPage videoId={post.videoId} youtubedescription={post.youtubedescription} />
+            <ContactPage />
+          </React.Fragment>
+        ))
+      ) : (
+        <p>No posts found with the specified tag.</p>
+      )}
+    </Layout>
+  );
 };
 
 export default Index;
