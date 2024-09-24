7b85b4c2a96e51fdd32ff5a423f51d41b2747036
VITALIY1986 <staslego1986@gmail.com>
2024-09-24 11:57:28 +0300

    sdgsdgsgdgsds

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -7,13 +7,15 @@ import { FormattedMessage } from "react-intl";
 
 import { IsOpenProps } from "@/components/layout/Layout";
 
+
+
 interface FooterProps extends IsOpenProps {
-	logo: string; // Убедитесь, что это свойство объявлено
+	
+	logo: string;
+	
 }
-
 const Footer: FC<FooterProps> = (props) => {
-	const { logo } = props; // Извлечение свойства logo из props
-
+	const { logo } = props;
 	return (
 		<div className={scss.footer}>
 			<div className="container">
@@ -37,14 +39,78 @@ const Footer: FC<FooterProps> = (props) => {
 								<FormattedMessage id="page.footer.block_1.title" />
 							</p>
 						</div>
-						{/* Остальная часть кода */}
+						<div className={scss.block}>
+							<h6>
+								<FormattedMessage id="page.footer.block_2.title" />
+							</h6>
+							<a className={scss.link__text} href="#">
+								<FormattedMessage id="page.footer.block_2.text_1" />
+							</a>
+							<a className={scss.link__text} href="#service">
+								<FormattedMessage id="page.footer.block_2.text_2" />
+							</a>
+							<a className={scss.link__text} href="#about">
+								<FormattedMessage id="page.footer.block_2.text_3" />
+							</a>
+							<a className={scss.link__text} href="#contact">
+								<FormattedMessage id="page.footer.block_2.text_4" />
+							</a>
+						</div>
+						<div className={scss.block}>
+							<h6>
+								<FormattedMessage id="page.footer.block_3.title" />
+							</h6>
+							<a className={scss.link__text} href="#service">
+								<FormattedMessage id="page.footer.block_3.text_1" />
+							</a>
+						
+						</div>
+						<div className={scss.block}>
+							<h6>
+								<FormattedMessage id="page.footer.block_4.title" />
+							</h6>
+							{/*<a className={scss.link__text} href="#">*/}
+							{/*	boss.armsport@gmail.com*/}
+							{/*</a>*/}
+							<a className={scss.link__text} href="tel:+40724955956">
+							+40724955956
+							</a>
+						
+						</div>
 					</div>
 					<hr />
-					{/* Нижняя часть кода */}
+					<div className={scss.footer__bottom}>
+						<div className={scss.copyright}>© 2019-2024 Cabinet ORL / Toate drepturile rezervate.</div>
+						<div className={scss.icons}>
+							<a
+								className={scss.link}
+								href="#"
+								target="_blank"
+								aria-label="Link to Instagram profile"
+							>
+								<InstagramIcon />
+							</a>
+							<a
+								className={scss.link}
+								href="#"
+								target="_blank"
+								aria-label="Link to WhatApp profile"
+							>
+								<WhatAppIcon />
+							</a>
+							<a
+								className={scss.link}
+								href="#"
+								target="_blank"
+								aria-label="Link to Telegram Call"
+							>
+								<TelegramIcon />
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 };
-
 export default Footer;
