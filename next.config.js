a8eacf0300999d5537ca735cd44eee1d272bc315
VITALIY1986 <staslego1986@gmail.com>
2024-10-01 14:15:05 +0300

    asghfhdjfjdf

--- next.config.js
+++ next.config.js
@@ -1,5 +1,59 @@
 /** @type {import("next").NextConfig} */
+
+
+
+
+
+
+
+
+
 const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.ctfassets.net',
+				port: '', // оставьте пустым, если не требуется
+				pathname: '/**', // разрешаем все пути
+			},
+		],
+	},
+	i18n: {
+		locales: ["ro"], // оставляем только один язык
+		defaultLocale: "ro", // назначаем его как основной
+		localeDetection: false // при необходимости
+	},
+
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [{ loader: "@svgr/webpack", options: { icon: true } }]
+		});
+
+		return config;
+	}
+};
+
+module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** const nextConfig = {
 	
 	reactStrictMode: true,
 	images: {
@@ -29,3 +83,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+*/
