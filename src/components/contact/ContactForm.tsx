6ca69fad8f1879adcd13e3d7b3c589dba2092f95
VITALIY1986 <staslego1986@gmail.com>
2024-11-17 19:18:44 +0200

    asdggadsa

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -205,7 +205,7 @@ const ContactForm: FC<ContactFormProps> = ({ className,programaretitle, programa
 							</div>
 
 							<div className={scss.textareaBx}>
-							<input
+							<textarea
     name="message"
     aria-labelledby="message"
     id="message"
